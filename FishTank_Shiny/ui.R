6536c1e60958d810ead40353c315174b29d546d3
fawda123 <mbafs2012@gmail.com>
2017-05-24 22:01:44 -0500

    changed header text of ui

--- FishTank_Shiny/ui.R
+++ FishTank_Shiny/ui.R
@@ -25,7 +25,7 @@ shinyUI(fluidPage(
       ),
       
       column(width = 10, 
-        h5('This application runs the FishTank model as part of the Coastal Gulf Ecology Model (CGEM).  CGEM calculates a set of bio-geo-chemical equations based on the Eldridge and Roelke model (Eldridge and Roelke, 2010). The equations have been enhanced by J. Lehrter (Lehrter, et al. 2014). A comprehensive light model has been added by B. Penta of the Naval Research Lab (NRL) (Penta et al., 2008). Hydrodynamics and transport were added by D.S. Ko of NRL (Ko et al., 2008). The model aims to explain the seasonal appearance of a large hypoxic zone off of the Louisiana continental shelf (LCS).  Input parameters are selected on the left tab and the model is run by clicking the button to the left.  Results are viewed as time series plots for 33 different state variables on the right tab.')
+        h5('This application runs the FishTank model as part of the Communitity General Ecosystem Model (CGEM).  CGEM calculates a set of bio-geo-chemical equations based on the Eldridge and Roelke model (Eldridge and Roelke 2010). The equations have been enhanced by J. Lehrter (Lehrter et al. 2014). A comprehensive light model has been added by B. Penta of the Naval Research Lab (NRL) (Penta et al., 2008). Hydrodynamics and transport were added by D.S. Ko of NRL (Ko et al. 2008). The model aims to explain the seasonal appearance of a large hypoxic zone off of the Louisiana continental shelf (LCS).  Input parameters are selected on the left tab and the model is run by clicking the button to the left.  Results are viewed as time series plots for 33 different state variables on the right tab.')
       )
      
     ),
